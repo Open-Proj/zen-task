19f30067ae0710954be624122b56c76fa8033c26
Zacharie Day <zachdayz@gmail.com>
2018-04-08 15:05:38 -0400

    Use four spaces like a real man

--- android/app/src/main/java/com/openproj/zentask/MainActivity.java
+++ android/app/src/main/java/com/openproj/zentask/MainActivity.java
@@ -11,28 +11,29 @@ import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  private static String TAG = "main";
+    private static String TAG = "main";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
 
-    NativeBinary bin = new NativeBinary(getApplicationContext());
-    if (!bin.isInstalled()) {
-      try {
-        bin.install();
-      } catch (IOException e) {
-        Log.e(TAG, "Error installing binary.");
-        e.printStackTrace();
-      }
+        NativeBinary bin = new NativeBinary(getApplicationContext());
+        if (!bin.isInstalled()) {
+            try {
+                bin.install();
+
+            } catch (IOException e) {
+                Log.e(TAG, "Error installing binary.");
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            bin.call();
+        } catch (IOException e) {
+            Log.e(TAG, "Error calling binary.");
+            e.printStackTrace();
+        }
     }
-
-    try {
-      bin.call();
-    } catch (IOException e) {
-      Log.e(TAG, "Error calling binary.");
-      e.printStackTrace();
-    }
-  }
 }
